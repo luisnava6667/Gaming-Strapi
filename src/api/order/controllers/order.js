b314c554558683853efcdf0050064163a02ee5c8
luisnava6667 <navaluisrodolfo@gmail.com>
2023-07-01 09:47:43 -0300

    first commit

---
+++ src/api/order/controllers/order.js
@@ -0,0 +1,51 @@
+'use strict'
+
+// @ts-ignore
+const stripe = require('stripe')(process.env.STRAPI_SECRET_KEY)
+function calcDiscountPrice(price, discount) {
+  if (!discount) return price
+  const discountAmount = (price * discount) / 100
+  const result = price - discountAmount
+  return result.toFixed(2)
+}
+/**
+ * order controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories
+
+module.exports = createCoreController('api::order.order', ({ strapi }) => ({
+  async paymentOrder(ctx) {
+    const { token, products, idUser, addressShipping } = ctx.request.body
+    let totalPayment = 0
+    products.forEach((product) => {
+      const priceTemp = calcDiscountPrice(
+        product.attributes.price,
+        product.attributes.discount
+      )
+      totalPayment += Number(priceTemp) * product.quantity
+    })
+    const charge = await stripe.charges.create({
+      amount: Math.round(totalPayment * 100),
+      currency: 'usd',
+      source: token.id,
+      description: `User ID: ${idUser}`
+    })
+    const data = {
+      products,
+      user: idUser,
+      totalPayment,
+      idPayment: charge.id,
+      addressShipping
+    }
+    const model = strapi.contentTypes['api::order.order']
+    const validData = await strapi.entityValidator.validateEntityCreation(
+      model,
+      data
+    )
+    const entry = await strapi.db
+      .query('api::order.order')
+      .create({ data: validData })
+    return entry
+  }
+}))
